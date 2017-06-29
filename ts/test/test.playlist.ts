2c2cdc25a65503d906b60743d6adec2faa9d3b88
Alan Wernick <alan.wernik@gmail.com>
2017-06-29 09:00:00 -0700

    Test fromURI method of factory

--- ts/test/test.playlist.ts
+++ ts/test/test.playlist.ts
@@ -2,12 +2,14 @@ import "mocha";
 import * as chai from "chai";
 let config = require('./helpers').config;
 let api = require('../api')(config.access_token, config.refreshToken);
-import Playlist, { PlaylistFactory } from "../playlist";
+let Playlist = require("../playlist").default;
+let PlaylistFactory = require('../playlist')['PlaylistFactory'];
 let should = chai.should();
 let expect = chai.expect;
 
 const VALID_USER_ID = 'spotify';
 const VALID_PLAYLIST_ID = '37i9dQZF1DXdgz8ZB7c2CP';
+const VALID_PLAYLIST_URI = `${VALID_USER_ID}:playlist:${VALID_PLAYLIST_ID}`;
 
 
 describe('Playlist', function() {
@@ -18,32 +20,62 @@ describe('Playlist', function() {
   })
 
   describe('factory', function() {
-    it('should throw error if REST client is not set', function() {
-      expect(() => {
+    let invalidateCacheAndReload = function() {
+      // Invalidate singletons
+      delete require.cache[require.resolve('../playlist')]
+
+      // Reload singleton classes
+      api = require('../api')(config.access_token, config.refreshToken);
+      PlaylistFactory = require('../playlist')['PlaylistFactory'];
+      Playlist = require("../playlist").default;
+    }
+
+    beforeEach(invalidateCacheAndReload);
+    afterEach(invalidateCacheAndReload);
+
+    describe('create()', function() {
+      it('should throw error if REST client is not set', function() {
+        expect(() => {
+          let playlist = PlaylistFactory.create('MyPlaylist', {
+            userId: VALID_USER_ID,
+            id: VALID_PLAYLIST_ID
+          })
+        }).to.throw(Error);
+      })
+
+      it('should not throw error if REST client is set', function() {
+        PlaylistFactory.setClient(api);
+        expect(() => {
+          let playlist = PlaylistFactory.create('MyPlaylist', {
+              userId: VALID_USER_ID,
+              id: VALID_PLAYLIST_ID
+          })
+        }).to.not.throw(Error);
+      })
+
+      it('should create valid instance of Playlist', function() {
+        PlaylistFactory.setClient(api);
         let playlist = PlaylistFactory.create('MyPlaylist', {
           userId: VALID_USER_ID,
           id: VALID_PLAYLIST_ID
         })
-      }).to.throw(Error);
-    })
-
-    it('should not throw error if REST client is set', function() {
-      PlaylistFactory.setClient(api);
-      expect(() => {
-        let playlist = PlaylistFactory.create('MyPlaylist', {
-            userId: VALID_USER_ID,
-            id: VALID_PLAYLIST_ID
-        })
-      }).to.not.throw(Error);
-    })
-
-    it('should create valid instance of Playlist', function() {
-      PlaylistFactory.setClient(api);
-      let playlist = PlaylistFactory.create('MyPlaylist', {
-        userId: VALID_USER_ID,
-        id: VALID_PLAYLIST_ID
+        playlist.should.be.instanceof(Playlist);
       })
-      playlist.should.be.instanceof(Playlist);
+    })
+
+    describe('fromUri()', function() {
+      it('should throw error if REST client is not set', function() {
+        expect(() => {
+          PlaylistFactory.fromUri(VALID_PLAYLIST_URI)
+        }).to.throw(Error);
+      })
+
+      it('should not throw error if REST client is set', function() {
+        PlaylistFactory.setClient(api);
+        expect(() => {
+          PlaylistFactory.fromUri(VALID_PLAYLIST_URI)
+        }).to.not.throw(Error);
+      })
     })
   })
   
