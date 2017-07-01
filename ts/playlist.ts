d5231f0e21b18f4fc77e7bd98d543ee4a1e63322
Alan Wernick <alan.wernik@gmail.com>
2017-07-01 10:10:24 -0700

    Add documentation for playlist

--- ts/playlist.ts
+++ ts/playlist.ts
@@ -9,6 +9,7 @@ interface PlaylistOptions {
   description?: string
 }
 
+/** Object representation for a Spotify playlist. **/
 class Playlist {
   public id: string | undefined
   public userId: string | undefined
@@ -19,6 +20,12 @@ class Playlist {
   public tracks: Array<Track>
   public api: any
 
+  /**
+   * @constructor
+   * @param {string} name - The name of the playlist
+   * @param {PlaylistOptions} options - The options for the playlist
+   * @param {SpotifyAPI} api - Instance of SpotifyAPI
+   */
   constructor(name: string, options: PlaylistOptions, api: any) {
     this.name = name;
     Object.assign(this, options);
@@ -26,6 +33,11 @@ class Playlist {
     this.tracks = [];
   }
 
+  /**
+   * Fetches information from Spotify API and loads it
+   * into the appropriate attributes
+   * @returns {Promise} status of the request to the Spotify API
+   */
   public load() {
     return new Promise<any>((resolve: any, reject: any) => {
       this.api.getPlaylist(this.userId, this.id)
@@ -44,12 +56,23 @@ class Playlist {
   }
 
 
-  public duplicate(options: any) {
+  /**
+   * Duplicates a playlist without saving it through the Spotify API
+   * @param {PlaylistOptions} options - options for the duplicate playlist
+   * @returns {Playlist} duplicate of the current playlist.
+   **/
+  public duplicate(options: PlaylistOptions) {
     let playlist = clone(this);
     Object.assign(this, options);
     return playlist;
   }
 
+  /**
+   * Saves a playlist using the Spotify API.
+   * If the playlist does not have a valid id, it first calls Playlist#create
+   * to generate it.
+   * @returns {Promise} status of the request to the Spotify API
+   */
   public save() {
     return new Promise((resolve, reject) => {
 
@@ -70,7 +93,13 @@ class Playlist {
     })
   }
 
+  /**
+   * Commits a playlist and its tracks to Spotify.
+   * @throws {Error} error - if playlist id is previously set.
+   */
   public create() {
+    // TODO: Assert id is blank before creating
+
     return new Promise((resolve, reject) => {
       this.api.createPlaylist(this.userId, this.name, {
         public: this.visible,
@@ -85,7 +114,11 @@ class Playlist {
     })
   }
 
-  /** Unfollowing your own playlist is the same as destroying them **/
+  /** 
+   * Unfollows a playlist for the current user.
+   * NOTE: Unfollowing your own playlist is the same as destroying them 
+   * @returns {Promise} status of the request to the Spotify API
+   * **/
   public unfollow() {
     return new Promise((resolve, reject) => {
       this.api.unfollowPlaylist(this.userId, this.id)
@@ -98,6 +131,11 @@ class Playlist {
     })
   }
 
+  /**
+   * Convert Spotify API track response into simpler Track objects,
+   * and store them in the current playlist.
+   * @param {Array<any>} items - API Track object array
+   */
   public _loadTracks(items: Array<any>) {
     if (typeof items === 'undefined' || !Array.isArray(items)) {
       throw new Error('Please provide an array of tracks to load');
@@ -123,6 +161,10 @@ class Playlist {
     }
   }
 
+  /**
+   * Adds the current tracks to the playlist using the Spotify API.
+   * @returns {Promise} - status of the Spotify API request
+   */
   public _saveTracks() {
     let uris: Array<string> = [];
     for(let track of this.tracks) {
@@ -143,15 +185,31 @@ class Playlist {
   }
 }
 
+
+/**
+ * Builder for PlaylistFactory singleton.
+ * PlaylistFactory facilitates the creation of playlist objects by
+ * injecting the Spotify API as a dependecy autmomatically.
+ */
 class PlaylistFactoryBuilder {
 
   /** Spotify REST client **/
   private client: any
 
+  /**
+   * Set the Spotify API client
+   * @param {SpotifyAPI} client - the Spotify API REST client
+   */
   public setClient(client: any) {
     this.client = client;
   }
 
+  /**
+   * Builds a Playlist instance with the given name and options.
+   * @param {String} name - the name for the playlist.
+   * @param {PlaylistOptions} options - the options for the new playlist
+   * @returns {Playlist} a new instance of Playlist
+   */
   public build(name: string, options: PlaylistOptions) {
     if(this.client == null) {
       throw new Error("Missing REST client. Call setClient first");
@@ -159,6 +217,11 @@ class PlaylistFactoryBuilder {
     return new Playlist(name, options, this.client);
   }
 
+  /**
+   * Builds a playlist from the given URI
+   * @param {String} uri - the Spotify URI for the new Playlist instance
+   * @returns {Playlist} a new instance of Playlist for the URI
+   */
   public fromUri(uri: string) {
     var [_, _, userId, kind, id] = uri.split(':');
     let playlist = this.build("NewPlaylist", {
