d2eafed0ab9c0ab927ab82d0eae248135f7f6917
Alan Wernick <Alan.Wernick@bmwna.com>
2017-07-06 10:50:48 -0700

    Change test process to only use env variables

--- ts/test/helpers.ts
+++ ts/test/helpers.ts
@@ -4,25 +4,17 @@ import * as path from "path";
 import Constants from "../constants";
 
 let API = require('../api');
-let CONFIG_PATH = Constants.CONFIG_PATH;
+require('dotenv').config();
 
 // Load config file, or create one
-let configFile: Buffer;
 let config: any;
-
-try {
-  configFile = fs.readFileSync(CONFIG_PATH);
-  config = JSON.parse(configFile.toString());
-} catch (err) {
-  fs.writeFileSync(CONFIG_PATH, '{}');
-  config = {};
-}
 
 // Overwrite config if in test ENV
 if(process.env.NODE_ENV == 'test') {
   config = {
     access_token: process.env.SPOTIFY_ACCESS_TOKEN,
-    refresh_token: process.env.SPOTIFY_REFRESH_TOKEN
+    refresh_token: process.env.SPOTIFY_REFRESH_TOKEN,
+    client_secret: process.env.SPOTIFY_CLIENT_SECRET
   }
 }
 
@@ -38,35 +30,16 @@ function refreshConfig() {
 
   // We can refresh a token in test env since we 
   // have client secret.
-  if(process.env.NODE_ENV == 'test') {
-    api.setClientSecret(process.env.SPOTIFY_CLIENT_SECRET);
-    return new Promise((resolve, reject) => {
-      api.refreshAccessToken()
-        .then((data: any) => {
-          api.setAccessToken(data.body.access_token);
-          resolve();
-        })
-        .catch((err: Error) => reject(err))
-    })
-  } 
-
-  // Client side can only be generated and not refreshed. Open
-  // a browser window to generate a new access token.
-  else {
-    return new Promise((resolve, reject) => {
-      api.generateAccessToken().then((token: string) => {
-        let new_config = {
-          "access_token": token
-        };
-        
-        config = {...config, ...new_config};
-        fs.writeFile(CONFIG_PATH, JSON.stringify(config), (error?: Error) => {
-          if(error) { return reject(error) }
-          resolve(config);
-        })
+  api.setClientSecret(config.client_secret);
+  api.setRefreshToken(config.refresh_token);
+  return new Promise((resolve, reject) => {
+    api.refreshAccessToken()
+      .then((data: any) => {
+        api.setAccessToken(data.body.access_token);
+        resolve();
       })
-    })
-  }
+      .catch((err: Error) => reject(err))
+  })
 }
 
 module.exports = {
