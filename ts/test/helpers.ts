79c29a071cfcc85a30c187c88de2349ebb98cf7a
Alan Wernick <Alan.Wernick@bmwna.com>
2017-07-06 10:34:56 -0700

    Use client secret and test env to refresh spotify access tokens for CI

--- ts/test/helpers.ts
+++ ts/test/helpers.ts
@@ -18,6 +18,13 @@ try {
   config = {};
 }
 
+// Overwrite config if in test ENV
+if(process.env.NODE_ENV == 'test') {
+  config = {
+    access_token: process.env.SPOTIFY_ACCESS_TOKEN,
+    refresh_token: process.env.SPOTIFY_REFRESH_TOKEN
+  }
+}
 
 const VALID_USER_ID = 'spotify';
 const VALID_PLAYLIST_ID = '37i9dQZF1DXdgz8ZB7c2CP';
@@ -27,20 +34,39 @@ const PERSONAL_PLAYLIST_ID = '3LMKpFlpPDC1QjE49DmDms';
 
 /** Generate a new access token and save it to our config fie **/
 function refreshConfig() {
-  return new Promise((resolve, reject) => {
-    let api = new API(config.access_token);
-    api.generateAccessToken().then((token: string) => {
-      let new_config = {
-        "access_token": token
-      };
-      
-      config = {...config, ...new_config};
-      fs.writeFile(CONFIG_PATH, JSON.stringify(config), (error?: Error) => {
-        if(error) { return reject(error) }
-        resolve(config);
+  let api = new API(config.access_token);
+
+  // We can refresh a token in test env since we 
+  // have client secret.
+  if(process.env.NODE_ENV == 'test') {
+    api.setClientSecret(process.env.SPOTIFY_CLIENT_SECRET);
+    return new Promise((resolve, reject) => {
+      api.refreshAccessToken()
+        .then((data: any) => {
+          api.setAccessToken(data.body.access_token);
+          resolve();
+        })
+        .catch((err: Error) => reject(err))
+    })
+  } 
+
+  // Client side can only be generated and not refreshed. Open
+  // a browser window to generate a new access token.
+  else {
+    return new Promise((resolve, reject) => {
+      api.generateAccessToken().then((token: string) => {
+        let new_config = {
+          "access_token": token
+        };
+        
+        config = {...config, ...new_config};
+        fs.writeFile(CONFIG_PATH, JSON.stringify(config), (error?: Error) => {
+          if(error) { return reject(error) }
+          resolve(config);
+        })
       })
     })
-  })
+  }
 }
 
 module.exports = {
